a6107f93951205d8d60456bd689ccab33392d4ab
punsaraprathibha <punsaraprathibhagpp@gmail.com>
2024-05-12 17:21:22 +0530

    Updated the file

--- dist/app.js
+++ dist/app.js
@@ -1,25 +1,56 @@
 "use strict";
-// function greet (name: string): string {
-//     return `Hello ${name}`;
-// }
-// console.log(greet("Javascript"));
 class Human {
     constructor(noOfLegs, noOfHands) {
         this.noOfLegs = noOfLegs;
         this.noOfHands = noOfHands;
     }
+    getNoOfLegs() {
+        return this.noOfLegs;
+    }
+    getNoOfHands() {
+        return this.noOfHands;
+    }
     setNoOfLegs(noOfLegs) {
         this.noOfLegs = noOfLegs;
     }
-    getNoOfLegs() {
-        return this.noOfLegs;
+    setNoOfHands(noOfHands) {
+        this.noOfHands = noOfHands;
     }
     run() {
         console.log("Human is Running..");
     }
 }
 class Employee extends Human {
+    constructor(noOfLegs, noOfHands, empCode, name) {
+        super(noOfLegs, noOfHands);
+        this.empCode = empCode;
+        this.empName = name;
+    }
+    getEmpCode() {
+        return this.empCode;
+    }
+    getEmpName() {
+        return this.empName;
+    }
+    setEmpCode(empCode) {
+        this.empCode = empCode;
+    }
+    setEmpName(empName) {
+        this.empName = empName;
+    }
+    run() {
+        console.log("Employee is Running..");
+    }
 }
-let human = new Human(2, 2);
-console.log(human.getNoOfLegs());
-human.run();
+let emp = new Employee(2, 2, 100, "Steve");
+console.log(emp.getEmpCode());
+console.log(emp.getEmpName());
+emp.setEmpCode(101);
+emp.setEmpName("Steve-2");
+console.log(emp.getEmpCode());
+console.log(emp.getEmpName());
+emp.setNoOfHands(2);
+emp.setNoOfLegs(2);
+console.log(emp.getNoOfHands());
+console.log(emp.getNoOfLegs());
+emp.run();
--- server/app.ts
+++ server/app.ts
@@ -1,38 +1,78 @@
-// function greet (name: string): string {
-//     return `Hello ${name}`;
-// }
-// console.log(greet("Javascript"));
-
 interface Animal {
     run(): void;
 }
-class Human implements Animal {
 
+class Human implements Animal {
     private noOfLegs: number;
     private noOfHands: number;
 
-    constructor(noOfLegs: number, noOfHands: number) {
+    public constructor(noOfLegs: number, noOfHands: number) {
         this.noOfLegs = noOfLegs;
         this.noOfHands = noOfHands;
     }
 
-    public setNoOfLegs(noOfLegs: number) {
+    public getNoOfLegs(): number {
+        return this.noOfLegs;
+    }
+
+    public getNoOfHands(): number {
+        return this.noOfHands;
+    }
+
+    public setNoOfLegs(noOfLegs: number): void {
         this.noOfLegs = noOfLegs;
     }
 
-    public getNoOfLegs(): number{
-        return this.noOfLegs;
+    public setNoOfHands(noOfHands: number): void {
+        this.noOfHands = noOfHands;
     }
-    run(): void {
-        console.log("Human is Running..");
+
+    public run(): void {
+        console.log("Human is Running..")
     }
 }
 
 class Employee extends Human {
 
+    private empCode: number;
+    private empName: string;
+
+    public constructor(noOfLegs: number, noOfHands: number, empCode: number, name: string) {
+        super(noOfLegs, noOfHands);
+        this.empCode = empCode;
+        this.empName = name;
+    }
+
+    public getEmpCode(): number{
+        return this.empCode;
+    }
+
+    public getEmpName(): string{
+        return this.empName;
+    }
+
+    public setEmpCode(empCode: number): void{
+        this.empCode = empCode;
+    }
+
+    public setEmpName(empName: string): void {
+        this.empName = empName;
+    }
+
+    public run(): void {
+        console.log("Employee is Running..")
+    }
 }
 
-let human = new Human(2, 2);
-console.log(human.getNoOfLegs());
-human.run();
-
+let emp = new Employee(2, 2,100, "Steve");
+console.log(emp.getEmpCode());
+console.log(emp.getEmpName());
+emp.setEmpCode(101);
+emp.setEmpName("Steve-2");
+console.log(emp.getEmpCode());
+console.log(emp.getEmpName());
+emp.setNoOfHands(2);
+emp.setNoOfLegs(2);
+console.log(emp.getNoOfHands());
+console.log(emp.getNoOfLegs());
+emp.run();
